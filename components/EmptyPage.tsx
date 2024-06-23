e1e9b62b9170ac4515412b2d744a2243dc3228bd
Fardeen S <fardeenclan@gmail.com>
2024-06-23 19:31:22 +0530

    added confietti and empty page

--- components/EmptyPage.tsx
+++ components/EmptyPage.tsx
@@ -1,8 +1,8 @@
 const EmptyPage = () => {
   return (
     <div className="flex w-full h-full items-center justify-center flex-col gap-5">
-      <h1 className="font-bold text-3xl">No Todo's Yet :(</h1>
-      <h1 className="font-bold text-3xl">Add Todos to be productive!</h1>
+      <h1 className="font-bold text-3xl">No Tasks Yet :(</h1>
+      <h1 className="font-bold text-3xl">Add Tasks to be productive!</h1>
     </div>
   );
 };
